93242049cb6a13cfbf00c11d727aeb2bb9d09bc9
khokhanileeza@gmai.com <leeza6234>
2023-05-02 00:06:49 +0530

    project

---
+++ lib/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color colorBG = Color(0xff15172b);
+Color colorOTPGenerator = Color(0xfff6db87);
+Color colorUnderLine = Color(0xfff6db87);
+Color colorTextField = Color(0xfffcf6ba);
+Color colorGeneratorOTPContainer1 = Color(0xffffe8b8);
+Color colorGeneratorOTPContainer2 = Color(0xfff6db87);
+
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'otp.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+       home: otp_home(),
+     )
+  );
+}
